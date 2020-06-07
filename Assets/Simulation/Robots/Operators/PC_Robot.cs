855634bd163ed1485668d139a748325373062dfd
Dushess0 <ilyabannila@gmail.com>
2020-06-07 22:56:35 +0200

    fixed UI, added two new buildings and one new robot

---
+++ Assets/Simulation/Robots/Operators/PC_Robot.cs
@@ -0,0 +1,13 @@
+﻿using Simulation.Robots;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_Robot : Robot
+{
+    public override int BuildIterations => int.MaxValue;
+
+    protected override int cointainer_size => int.MaxValue;
+
+ 
+}
